5169ccf4e4b12110bd7100e91d8171177ff7cf5f
nodeexcel <atul.etech2011@gmail.com>
2024-07-01 18:01:50 +0530

    moved constant from content-disposition/index to constant file

---
+++ packages/content-disposition/src/constant.ts
@@ -0,0 +1,8 @@
+const PARAM_REGEXP =
+  // biome-ignore lint/suspicious/noControlCharactersInRegex: <explanation>
+  /;[\x09\x20]*([!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*=[\x09\x20]*("(?:[\x20!\x23-\x5b\x5d-\x7e\x80-\xff]|\\[\x20-\x7e])*"|[!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*/g
+const TEXT_REGEXP = /^[\x20-\x7e\x80-\xff]+$/
+const TOKEN_REGEXP = /^[!#$%&'*+.0-9A-Z^_`a-z|~-]+$/
+
+const EXT_VALUE_REGEXP =
+  /^([A-Za-z0-9!#$%&+\-^_`{}~]+)'(?:[A-Za-z]{2,3}(?:-[A-Za-z]{3}){0,3}|[A-Za-z]{4,8}|)'((?:%[0-9A-Fa-f]{2}|[A-Za-z0-9!#$&+.^_`|~-])+)$/
